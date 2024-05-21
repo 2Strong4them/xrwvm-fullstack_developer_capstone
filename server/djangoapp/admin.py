364e58150972ed37d3be42499dd565644fdda58a
2Strong4them <91446777+2Strong4them@users.noreply.github.com>
2024-05-21 15:31:51 -0400

    finalv4

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -11,3 +11,25 @@
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+# Inline class for CarModel
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1  # Number of extra forms to display in the inline form
+
+# Admin class for CarMake
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')  # Columns to display in the list view
+    inlines = [CarModelInline]  # Include CarModel inline
+
+# Admin class for CarModel
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')  # Columns to display in the list view
+    list_filter = ('car_make', 'type', 'year')  # Filters to display in the sidebar
+    search_fields = ('name', 'car_make__name')  # Fields to search
+
+# Register models with the admin site
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
